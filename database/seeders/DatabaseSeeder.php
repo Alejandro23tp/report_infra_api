639871d2fa84ff49c0e6d9a2dbc65b36df94fb73
Alejandro23tp <alejandrolias84@gmail.com>
2025-05-13 22:01:42 -0500

    seeder admin - user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,21 @@ class DatabaseSeeder extends Seeder
             'cedula' => '1234567890',
             'direccion' => 'Test Address',
             'password' => Hash::make('password'),
-            'rol' => 'usuario',
+            'rol' => 'admin',
             'email_verified_at' => now(),
             'remember_token' => 'zWk5NG9MY7',
         ]);
+
+        User::create([
+            'nombre' => 'Alejandro',
+            'email' => 'alejandrolias84@gmail.com',
+            'cedula' => '2400321622',
+            'direccion' => 'Libertad',
+            'password' => Hash::make('1234567890'),
+            'rol' => 'usuario',
+            'email_verified_at' => now(),
+            'remember_token' => 'zWk5NG9MY8',
+        ]);
     }
+    
 }
